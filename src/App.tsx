a0066ee652c84f44da7958de92d5c0bb77a4302b
evripant <evripidispantelaios@gmail.com>
2022-11-02 16:05:42 +0200

    feature: add pop-up notification when adding an item to shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { RootState } from './store/index';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+import { RootState, AppDispatch } from './store/index';
+
+let isInitial = true;
 
 function App() {
+  const dispatch: (...args: unknown[]) => Promise<void> =
+    useDispatch<AppDispatch>();
+
   const showCart = useSelector<RootState, boolean>(
     (state) => state.ui.cartIsVisible
   );
+  const cart = useSelector<
+    RootState,
+    {
+      items: unknown[];
+      totalQuantity: number;
+    }
+  >((state) => state.cart);
+  const notification = useSelector<
+    RootState,
+    { status: string; title: string; message: string }
+  >((state) => state.ui.notification);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
